9489194c22106a682643dbc3e055dda0d44321fa
Leofront-end <ls171258@gmail.com>
2025-09-10 19:31:14 -0300

    feat e styles: arrumando a particula e o forms

--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
 document.addEventListener("DOMContentLoaded", () => {
     tsParticles.load("particles-js", {
         particles: {
-            number: { value: 80 },
+            number: { value: 200 },
             size: { value: 3 },
             move: { enable: true, speed: 2 },
             line_linked: { enable: true, color: "#00bfff" },
@@ -11,11 +11,11 @@ document.addEventListener("DOMContentLoaded", () => {
             events: {
                 onHover: {enable: true, mode: "repulse"},
                 onClick: {enable: true, mode: "push"}
-            }
-        },
-        modes: {
-            repulse: {distance: 100},
-            push: {quantity: 4}
+            },
+            modes: {
+                repulse: {distance: 100},
+                push: {quantity: 4}
+            },
         },
         
         detectRetina: true
