0755ba1e5814896ad71cc4c387fcf8fcf086adf1
Leofront-end <ls171258@gmail.com>
2025-09-10 19:12:48 -0300

    feat: adicicionando particula particle.js

---
+++ js/script.js
@@ -0,0 +1,23 @@
+document.addEventListener("DOMContentLoaded", () => {
+    tsParticles.load("particles-js", {
+        particles: {
+            number: { value: 80 },
+            size: { value: 3 },
+            move: { enable: true, speed: 2 },
+            line_linked: { enable: true, color: "#00bfff" },
+            color: { value: "#00bfff" }
+        },
+        interactivity: {
+            events: {
+                onHover: {enable: true, mode: "repulse"},
+                onClick: {enable: true, mode: "push"}
+            }
+        },
+        modes: {
+            repulse: {distance: 100},
+            push: {quantity: 4}
+        },
+        
+        detectRetina: true
+    })}
+)
